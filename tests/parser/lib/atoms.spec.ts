95a9c8e8e5ef94639c89510d9b05d694428391fa
prenaissance <alexandru.andries@isa.utm.md>
2023-02-27 22:29:09 +0200

    test(parser): added tests for parser building blocks

--- tests/parser/lib/atoms.spec.ts
+++ tests/parser/lib/atoms.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { TokenType } from "../../../src/lexer/tokens";
 import lex from "../../../src/lexer";
 import P from "../../../src/parser/lib";
+import { getValue } from "../../../src/parser/lib/parser";
 
 const expressionTokens = lex("1 + 2 * 3 - 4 / 5");
 const assignmentTokens = lex('a = "str"');
@@ -10,14 +11,23 @@ describe("parser atoms", () => {
   it("should parse tokens", () => {
     const result = P.token(TokenType.Number).run(expressionTokens);
     expect(result.isError).toBe(false);
-    const assigmnentResult = P.token(TokenType.Identifier).run(
+    const assignmentResult = P.token(TokenType.Identifier).run(
       assignmentTokens,
     );
-    expect(assigmnentResult.isError).toBe(false);
+    expect(assignmentResult.isError).toBe(false);
   });
 
   it("should fail on unexpected tokens", () => {
     const result = P.token(TokenType.Number).run(assignmentTokens);
     expect(result.isError).toBe(true);
+  });
+
+  it("should parse numbers", () => {
+    const result = P.number.run(expressionTokens);
+    expect(result.isError).toBe(false);
+    expect(getValue(result)).toBe(1);
+
+    const failedResult = P.number.run(assignmentTokens);
+    expect(failedResult.isError).toBe(true);
   });
 });
--- tests/parser/lib/parser.spec.ts
+++ tests/parser/lib/parser.spec.ts
@@ -2,5 +2,33 @@ import { describe, expect, it } from "vitest";
 import { TokenType } from "../../../src/lexer/tokens";
 import lex from "../../../src/lexer";
 import P from "../../../src/parser/lib";
+import { getValue } from "../../../src/parser/lib/parser";
 
-describe.todo("parser combinatorics");
+const expressionTokens = lex("1 + 2 * 3 - 4 / 5");
+
+describe("parser lib", () => {
+  it("should map parser results", () => {
+    const parser = P.string
+      .map((value) => value + "b")
+      .map((value) => value + "c")
+      .map((value) => value + "d");
+    const result = parser.run(lex('"aAa"'));
+    expect(result.isError).toBe(false);
+    expect(getValue(result)).toBe("aAabcd");
+  });
+
+  it("should stop propagation of map on errors", () => {
+    let sideEffect = false;
+
+    const parser = P.string
+      .map((value) => {
+        sideEffect = true;
+        return value + "b";
+      })
+      .map((value) => value + "c");
+
+    const result = parser.run(lex("123"));
+    expect(result.isError).toBe(true);
+    expect(sideEffect).toBe(false);
+  });
+});
